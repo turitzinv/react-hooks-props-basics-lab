9296b2a09ffa1defb6249b7b510e8bfa6a9a27bc
Victor Turitzin <turitzinv@gmail.com>
2022-02-05 19:20:22 -0500

    commit tree 4798f8b79b73a19a8f984a79426962686e410ea1

---
+++ src/components/Links.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Links(props) {
+  return (
+    <h3>Links
+      <a href='https://github.com/liza'>https://github.com/liza</a>
+      <a href='https://www.linkedin.com/in/liza/'>https://www.linkedin.com/in/liza/</a>
+    </h3>
+  )
+}
+export default Links;
